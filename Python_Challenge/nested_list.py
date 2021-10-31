6a81b07ec5e9782bc08e2958e6ee6053d98462a0
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-10-31 11:46:17 +0100

    new challenge from hackerrank

---
+++ Python_Challenge/nested_list.py
@@ -0,0 +1,10 @@
+marksheet = [[input(), float(input())] for _ in range(int(input()))]
+scores =  { score for _, score in marksheet}
+second_lowest = sorted(scores)[1]
+second_lowest_names = []
+for name, score in marksheet:
+    if score == second_lowest:
+        second_lowest_names.append(name)
+for name in sorted(second_lowest_names):
+    print(name)
+
