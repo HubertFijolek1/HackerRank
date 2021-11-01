b42f7f0ed72a94e5c5a809589254ef0fd4b170f2
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-01 08:37:02 +0100

    new challenge

---
+++ Python_Challenge/swap_case.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    new_string = []
+    for i in s:
+        if i == i.upper():
+            new_string.append(i.lower())
+        elif i == i.lower():
+            new_string.append(i.upper())
+        else:
+            new_string.append(i) 
+    zdanie = ('').join(new_string) 
+    return zdanie
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
