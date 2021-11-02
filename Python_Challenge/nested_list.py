f8b609afd05e7ac79602dc275c4cda102976462a
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-02 19:32:31 +0100

    little change

--- Python_Challenge/nested_list.py
+++ Python_Challenge/nested_list.py
@@ -2,7 +2,6 @@ marksheet = [[input(), float(input())] for _ in range(int(input()))]
 scores =  { score for _, score in marksheet}
 second_lowest = sorted(scores)[1]
 second_lowest_names = []
-
 for name, score in marksheet:
     if score == second_lowest:
         second_lowest_names.append(name)
