22fb69bd1c7036b0e627aa5a3941f0556bbaeac5
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-13 10:26:07 +0100

    adding task content

--- Python_Challenge/mutations.py
+++ Python_Challenge/mutations.py
@@ -1,3 +1,6 @@
+# Task
+# Read a given string, change the character at a given index and then print the modified string.
+
 def mutate_string(string, position, character):
     zdanie = string.split(' ')
     zdanie.insert(position, character)
