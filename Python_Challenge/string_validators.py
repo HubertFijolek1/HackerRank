5a8296d9fbaea8e191b7c31a52f60829d66f7403
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-13 10:29:46 +0100

    adding task content

--- Python_Challenge/string_validators.py
+++ Python_Challenge/string_validators.py
@@ -1,3 +1,9 @@
+# In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if S has any digits. Otherwise, print False.
+# In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if S has any uppercase characters. Otherwise, S print False.
+
 s = input()
 print(any(c.isalnum()  for c in s))
 print(any(c.isalpha() for c in s))
