a70b772629065656ce55e97ee6c39abbf0fb1ed8
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-13 10:22:26 +0100

    adding task content

--- Python_Challenge/finding_percentage.py
+++ Python_Challenge/finding_percentage.py
@@ -1,3 +1,16 @@
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+# Sample Input 0
+
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# Sample Output 0
+
+# 56.00
 student_marks = {}
 for _ in range(int(input())):
     name, *line = input().split()
