2f347c4114eaff35cfab716302c83cdf2b694540
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-15 09:18:27 +0100

    adding task content

--- 3_Months_Preparation_Kit/Plus_Minus.py
+++ 3_Months_Preparation_Kit/Plus_Minus.py
@@ -1,3 +1,11 @@
+# Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+
+# Print the following 3 lines, each to 6 decimals:
+
+# proportion of positive values
+# proportion of negative values
+# proportion of zeros
+
 n = float(input())
 lst = [int(x) for x in input().split()]
 print (format(len([x for x in lst if x > 0])/n, ".6f"))
