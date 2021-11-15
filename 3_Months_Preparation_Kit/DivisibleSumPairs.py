a5395ec53b7b855eae0d33b1516f0f4a817ca5d0
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-15 09:32:50 +0100

    adding task content

--- 3_Months_Preparation_Kit/DivisibleSumPairs.py
+++ 3_Months_Preparation_Kit/DivisibleSumPairs.py
@@ -2,15 +2,13 @@
 
 import os
 
-#
-# Complete the 'divisibleSumPairs' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. INTEGER_ARRAY ar
-#
+
+# Given an array of integers and a positive k, determine the number of (i,j) pairs where i<j and ar[i]+ ar[j] is divisible by k.
+
+#Example
+#ar = [1,2,3,4,5,6]
+#k = 5
+#Three pairs meet the criteria: [1,4], [2,3] and [4,6].
 
 def divisibleSumPairs(n, k, ar):
     # Write your code here
