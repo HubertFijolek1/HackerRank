36c3093a599092bcf05fe51b8b8f0914a6511437
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-19 17:53:53 +0100

    adding task content

--- 3_Months_Preparation_Kit/flipping_bits.py
+++ 3_Months_Preparation_Kit/flipping_bits.py
@@ -1,5 +1,6 @@
 # You will be given a list of 32 bit unsigned integers. Flip all the bits (1 ->0 and 0->1 ) and return the result as an unsigned integer.
 
+
 for _ in range(int(input())):
     s = 2**32 ^ int(input())
     t = str(bin(s))[2:]
