fcb161cb472985b5f3d131fce23281d30e257942
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-21 23:10:40 +0100

    updating task

--- Python_Challenge/Designer_Door_Math.py
+++ Python_Challenge/Designer_Door_Math.py
@@ -1,3 +1,4 @@
+
 # Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
 
 # Mat size must be NxM. ( N is an odd natural number, and M is 3  times N.)
