2be31225793e3ebacfe297907a416914b55ef812
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-23 17:03:30 +0100

    adding task content

--- 3_Months_Preparation_Kit/pangrams.py
+++ 3_Months_Preparation_Kit/pangrams.py
@@ -1,10 +1,6 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 #
 # Complete the 'pangrams' function below.
@@ -12,7 +8,7 @@ import sys
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
 #
-
+# A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 def pangrams(s):
     # Write your code here
     temp = set(s.lower()) - set(" ")
