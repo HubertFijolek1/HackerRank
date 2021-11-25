93185a0b49be2a246ce8434731537fbc05736f7f
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-25 18:23:09 +0100

    adding new task

--- 3_Months_Preparation_Kit/mars_exploration.py
+++ 3_Months_Preparation_Kit/mars_exploration.py
@@ -8,6 +8,13 @@ import os
 # The function accepts STRING s as parameter.
 #
 
+# Letters in some of the SOS messages are altered by cosmic radiation during transmission. Given the signal received by Earth as a string, , determine how many letters of the SOS message have been changed by radiation.
+
+# Example
+#s = 'SOSTOT'
+
+# The original message was SOSSOS. Two of the message's characters were changed in transit.
+
 def marsExploration(s):
     res = 0
     for ind in range(0, len(s), 3):
