9e7b3bd7c8d494a5f791a585a4499b43a563f166
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-26 20:19:32 +0100

    adding task content

--- 3_Months_Preparation_Kit/subarray_division.py
+++ 3_Months_Preparation_Kit/subarray_division.py
@@ -15,7 +15,13 @@ import sys
 #  2. INTEGER d
 #  3. INTEGER m
 #
+# Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
 
+# Lily decides to share a contiguous segment of the bar selected such that:
+
+# The length of the segment matches Ron's birth month, and,
+# The sum of the integers on the squares is equal to his birth day.
+# Determine how many ways she can divide the chocolate.
 def birthday(s, d, m):
     # Write your code here
     i = 0
