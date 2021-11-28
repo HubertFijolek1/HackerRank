d36275600070be21dbcd0001da3da64468f2bb56
Hubert Fijołek <hubertfijolek1@gmail.com>
2021-11-28 09:25:56 +0100

    adding new task

---
+++ 3_Months_Preparation_Kit/Migratory_birds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    count = arr.count(arr[0])
+    set1 = set(arr)
+    list1 = []
+    for i in set1:
+        if arr.count(i)>count and i not in list1:
+            count = arr.count(i)
+            max = i
+    return max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
